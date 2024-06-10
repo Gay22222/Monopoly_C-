2ded9a87d9a69e98ffe69ce55c4c32502035fec2
Gay22222 <thongditno17@gmail.com>
2024-06-10 20:23:51 +0700

    update Monopoly

---
+++ Monopoly/Monopoly_Client/Monopoly_Client/ConnectToServer.cs
@@ -0,0 +1,84 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Windows.Forms;
+
+namespace Monopoly_Client
+{
+    public partial class ConnectToServer : Form
+    {
+        private Client m_client;
+        public ConnectToServer()
+        {
+            InitializeComponent();
+            //GetLocalIPv4Address();
+        }
+
+        //private void GetLocalIPv4Address()
+        //{
+        //    try
+        //    {
+        //        string hostName = Dns.GetHostName();
+        //        IPAddress[] addresses = Dns.GetHostAddresses(hostName);
+        //        foreach (IPAddress address in addresses)
+        //        {
+        //            if (address.AddressFamily == AddressFamily.InterNetwork)
+        //            {
+        //                ipAddressBox.Text = address.ToString();
+        //                break;
+        //            }
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine("Không thể lấy địa chỉ IPv4: " + ex.Message);
+        //    }
+        //}
+
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            // Check if the user entered anything in the text box, entered too much, or entered command.
+            if (nameBox.Text.Length == 0)
+            {
+                MessageBox.Show("Vui lòng nhập tên để kết nối tới server.", "Lỗi");
+                return;
+            }
+            if (nameBox.Text.Length > 21)
+            {
+                MessageBox.Show("Ten khong duoc qua 20 ki tu.", "Loi");
+                return;
+            }
+            if (nameBox.Text.Contains(','))
+            {
+                MessageBox.Show("Vui long nhap ten khong co ki tu dac biet.", "Loi");
+                return;
+            }
+            // Create a client.
+            if (m_client == null)
+            {
+                m_client = new Client();
+            }
+            m_client.Name = nameBox.Text;
+            this.Text = "Dang ket noi...";
+            // Connect to the server. Was it a success?
+            if (m_client.Connect("127.0.0.1"))
+            {
+                // Show a message upon successful connection.
+                this.Text = "Thanh cong";
+                MessageBox.Show("Da ket noi thanh cong!");
+                // Set the client's name to the name they connected with.
+                // Open the main window, then close this window.
+                this.Hide();
+                MainWindow main = new MainWindow(m_client);
+                main.ShowDialog();
+                this.Close();
+            }
+            else
+            {
+                this.Text = "Kêt nối tới Server";
+                m_client.RefreshSocket();
+            }
+        }
+
+        
+    }
+}
---
+++ Monopoly/Monopoly_Server/Monopoly_Server/Server.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Monopoly_Server
+{
+    partial class Server
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lst_process = new ListView();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // lst_process
+            // 
+            lst_process.Location = new Point(31, 54);
+            lst_process.Name = "lst_process";
+            lst_process.Size = new Size(741, 384);
+            lst_process.TabIndex = 0;
+            lst_process.UseCompatibleStateImageBehavior = false;
+            lst_process.View = View.List;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(31, 36);
+            label1.Name = "label1";
+            label1.Size = new Size(82, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Server process";
+            // 
+            // Server
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(lst_process);
+            Name = "Server";
+            Text = "Server";
+            Load += Server_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListView lst_process;
+        private Label label1;
+    }
+}
